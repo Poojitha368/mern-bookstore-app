19e3cd8e98f143d4abdf155122685329d9bd5c2c
Poojitha368 <poojithareddy2662004@gmail.com>
2025-07-29 16:41:06 +0530

    mongodb url initialization

---
+++ backend/config.js
@@ -0,0 +1,6 @@
+export const PORT = 5555;
+
+// mongodb database
+export const mongoDBURL = 
+// password is p@$$word (p@$$word → p%40%24%24word)
+'mongodb+srv://root:p%40%24%24word@bookstore.xvopokj.mongodb.net/books-collection?retryWrites=true&w=majority&appName=bookstore'
